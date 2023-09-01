e02f970408a26e3c72d506d1fe9bc76a57003713
akumarjh <82668537+akumarjh@users.noreply.github.com>
2023-09-01 18:16:33 -0500

    MIG-4 Food Catalog micro service

---
+++ foodcatalog/src/main/java/com/miggy/foodcatalog/FoodcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.miggy.foodcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcatalogApplication.class, args);
+	}
+
+}
---
+++ foodcatalog/src/test/java/com/miggy/foodcatalog/FoodcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miggy.foodcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
